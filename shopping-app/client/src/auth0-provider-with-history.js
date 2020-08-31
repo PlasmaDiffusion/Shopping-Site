809b8a83ad40ddaafd29669e4d6766310dc322fa
PlasmaDiffusion <Scott50000@gmail.com>
2020-08-31 11:40:04 -0400

    Proper packages set

--- shopping-app/client/src/auth0-provider-with-history.js
+++ shopping-app/client/src/auth0-provider-with-history.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+//This will make React Router Hooks available to Auth0Provider
 const Auth0ProviderWithHistory = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -23,5 +24,11 @@ const Auth0ProviderWithHistory = ({ children }) => {
     </Auth0Provider>
   );
 };
+
+/*
+You use the onRedirectCallback() method to handle the event where Auth0 redirects your users from the Auth0 Universal Login page to your React application.
+You use the useHistory() hook to get the history object from React Router.
+You use the history.push() method to take users back to the route they intended to access before authentication.
+*/
 
 export default Auth0ProviderWithHistory;
