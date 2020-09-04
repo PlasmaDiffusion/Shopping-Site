8204e2ec7e016e0ae666b7129b69ac1fdb664434
PlasmaDiffusion <Scott50000@gmail.com>
2020-09-04 12:41:58 -0400

    Sequlize connection closes after tests, and test should exit

--- shopping-app/test/test.js
+++ shopping-app/test/test.js
@@ -5,8 +5,19 @@ const { Op } = require("sequelize");
 
 models = sequelize.models;
 
+var tests = 0;
+
+// Will run after every test in every file
+afterEach(function () {
+  tests++;
+  if (tests >= 4) {
+    sequelize.close();
+    console.log("Sequlize connection closed");
+  }
+});
+
 //ShopItem read testing -----------------------------------------------------------------------------------
-describe("The shopItem model object using find", async function () {
+describe("The shopItem model object using find", function () {
   it("should not be null with id 1", async function () {
     const shopItem = await models.shopItem.findByPk(1);
 
@@ -14,7 +25,7 @@ describe("The shopItem model object using find", async function () {
   });
 });
 
-describe("The shopItem model object using findAll", async function () {
+describe("The shopItem model object using findAll", function () {
   it("should not be null", async function () {
     const shopItems = await models.shopItem.findAll();
 
@@ -27,7 +38,7 @@ describe("The shopItem model object using findAll", async function () {
     });
 });
 
-describe("The shopItem model object using findAll with LIKE", async function () {
+describe("The shopItem model object using findAll with LIKE", function () {
   it("should get multiple items", async function () {
     // SELECT * FROM shopItem WHERE name LIKE %search keyword%
     var foundItems = await models.shopItem.findAll({
